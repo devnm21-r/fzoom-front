48c60a8627783670980d1b8c9cd27ef2b6ee5dbc
ayeItsVinay <69321432+ayeItsVinay@users.noreply.github.com>
2021-09-27 16:15:47 +0530

    Add files via upload

---
+++ src/components/HomeStart.js
@@ -0,0 +1,89 @@
+import React from "react";
+
+//material-ui
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+
+// import cover from "../images/food_upscaled.png";
+import cover from "../images/cafe.jpg";
+
+
+const useStyles = makeStyles((theme) => ({
+  presentation: {
+    display: "flex",
+    width: "90%",
+    margin: "auto",
+    minHeight: "80vh",
+    alignItems: "center",
+    // eslint-disable-next-line
+    ["@media (max-width:1024px)"]: {
+      flexDirection: "column",
+    },
+  },
+  introduction: {
+    flex: 1,
+    paddingLeft: 60,
+    height: "340px",
+  },
+  safeFood: {
+    fontSize: 41,
+    fontWeight: 400,
+    marginBottom: 20,
+  },
+  delivery: {
+    color: "#157a21",
+    fontSize: 45,
+    fontWeight: "bold",
+    marginTop: -30,
+    marginBottom: 20,
+  },
+  paragraph: {
+    width: 400,
+    fontSize: 14.5,
+  },
+  cover: {
+    flex: 1,
+    display: "flex",
+    justifyContent: "center",
+    height: "72vh",
+  },
+  coverImg: {
+    height: "100%",
+  },
+  ctaOrder: {
+    fontSize: 18,
+    backgroundColor: "#f7a692",
+    marginTop: 30,
+  },
+}));
+
+const HomeStart = () => {
+  const classes = useStyles();
+  return (
+    <section className={classes.presentation}>
+      <div className={classes.introduction}>
+        <Typography className={classes.safeFood} noWrap>
+          Restaurant processes,
+        </Typography>
+        <Typography className={classes.delivery} noWrap>
+          MADE EASIER
+        </Typography>
+        <Typography variant="body2" className={classes.paragraph}>
+          Now, be able to order you're food even before you reach
+          the restaurant! 
+          You're time ad effort is precious to us!
+        </Typography>
+        <Button variant="outlined" className={classes.ctaOrder}>
+          ORDER NOW
+        </Button>
+      </div>
+      <div className={classes.cover}>
+        <img src={cover} alt="restaurant" className={classes.coverImg} />
+      </div>
+    </section>
+  );
+};
+
+
+export default React.memo(HomeStart);
